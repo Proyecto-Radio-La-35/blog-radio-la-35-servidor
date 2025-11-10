1d0facc0b8d894be21da95a51e50e8d56d76a7fd
Gael De Luca <gaeldlk@gmail.com>
2025-11-10 04:25:04 -0300

    feat: Permite creación y almacenamiento de contenido

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.js";
+import contenidoRoutes from "./routes/contenido.js";
 
 dotenv.config();
 
@@ -9,13 +10,14 @@ const app = express();
 
 app.use(cors({
   origin: ["http://localhost:3000", "https://radio-la-35.netlify.app"],
-  methods: ["GET", "POST"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
 
 app.use(express.json());
 
 app.use("/auth", authRoutes);
+app.use("/contenido", contenidoRoutes);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Servidor iniciado en puerto ${PORT}`));
