7379d443053243346458e3f3270a2baad8280dc8
Gael De Luca <gaeldlk@gmail.com>
2025-11-10 00:28:20 -0300

    feat: Agrega CORS al backend

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: ["http://localhost:3000", "https://radio-la-35.netlify.app"],
+  methods: ["GET", "POST"],
+  credentials: true,
+}));
+
 app.use(express.json());
 
 app.use("/auth", authRoutes);
